3054fefea422171436dde3ea5302ac8145d9c61e
Enol García Heres <Enolgarciiaa@gmail.com>
2025-03-27 01:08:43 +0100

    mejoramos responsive móvil

--- js/navbar.js
+++ js/navbar.js
@@ -1,18 +1,8 @@
-let currentIndex = 0;
 
-function slideCarousel(direction) {
-  const carousel = document.getElementById('iconCarousel');
-  const columns = carousel.querySelectorAll('.icon-column');
-  const totalColumns = columns.length;
-  const visibleColumns = 3;
+  const hamburger = document.getElementById('hamburger');
+  const navigation = document.getElementById('navigation');
 
-  currentIndex += direction;
+  hamburger.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+  });
 
-  if (currentIndex < 0) currentIndex = 0;
-  if (currentIndex > totalColumns - visibleColumns) {
-    currentIndex = totalColumns - visibleColumns;
-  }
-
-  const columnWidth = columns[0].offsetWidth + 20; // 20px margen horizontal aprox
-  carousel.style.transform = `translateX(-${currentIndex * columnWidth}px)`;
-}
