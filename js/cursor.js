e8132649d9747064dd2f66e973ab737ea443e938
Enol García Heres <Enolgarciiaa@gmail.com>
2025-03-27 04:39:11 +0100

    preparamos formulario

---
+++ js/cursor.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cursor = document.createElement("div");
+    cursor.id = "custom-cursor";
+    cursor.textContent = "EG";
+    document.body.appendChild(cursor);
+
+    document.addEventListener("mousemove", (e) => {
+      cursor.style.left = `${e.clientX}px`;
+      cursor.style.top = `${e.clientY}px`;
+    });
+  });
